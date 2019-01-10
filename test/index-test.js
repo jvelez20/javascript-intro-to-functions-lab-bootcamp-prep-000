0e32add5df10a8581df8a1e787d27d89b3c9631b
Jason Velez <jvelez20@yahoo.com>
2019-01-10 18:44:52 +0000

    Automatically backed up by Learn

--- test/index-test.js
+++ test/index-test.js
@@ -12,7 +12,7 @@ describe('whisper(string)', function() {
 })
 
 describe('logShout(string)', function() {
-  it('calls console.log() its one argument in all caps', function() {
+  return('calls console.log() its one argument in all caps', function() {
     const spy = expect.spyOn(console, 'log').andCallThrough()
 
     logShout('hello')
