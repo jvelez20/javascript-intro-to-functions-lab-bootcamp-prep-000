f989b4fc9b545b59699ead416461e25ffcd8e87c
Jason Velez <jvelez20@yahoo.com>
2019-01-10 19:01:53 +0000

    Automatically backed up by Learn

--- test/index-test.js
+++ test/index-test.js
@@ -6,7 +6,7 @@ describe('shout(string)', function() {
 })
 
 describe('whisper(string)', function() {
-  return('recieves one argument and returns it in all lowercase', function() {
+  it('recieves one argument and returns it in all lowercase', function() {
     expect(whisper('HELLO')).toEqual('hello')
   })
 })
