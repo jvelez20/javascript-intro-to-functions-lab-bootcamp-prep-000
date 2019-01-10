e5baedcfd9a84a936c1c8e62d411b7a0b9192de2
Jason Velez <jvelez20@yahoo.com>
2019-01-10 19:05:34 +0000

    Automatically backed up by Learn

--- test/index-test.js
+++ test/index-test.js
@@ -6,7 +6,7 @@ describe('shout(string)', function() {
 })
 
 describe('logWhisper(string)', function() {
-  return('ralls console.log() its one argument in all lowercase', function()  {
+  return('calls console.log() its one argument in all lowercase', function()  {
     expect(whisper('HELLO')).toEqual('hello')
   })
 })
