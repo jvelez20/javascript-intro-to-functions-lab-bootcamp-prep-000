9578a5fbaff8acdb7a66e2bd4405048fa39fe8dc
Jason Velez <jvelez20@yahoo.com>
2019-01-10 18:47:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,4 +4,14 @@ function shout(string) {
 function whisper(string) {
   return string.toLowerCase()
 }
+describe('logShout(string)', function() {
+  return('calls console.log() its one argument in all caps', function() {
+    const spy = expect.spyOn(console, 'log').andCallThrough()
 
+    logShout('hello')
+
+    expect(spy).toHaveBeenCalledWith('HELLO')
+
+    console.log.restore()
+  })
+})
