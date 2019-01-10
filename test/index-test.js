a2e81ea96b12288ce1b03a4da44225e852c7e554
Jason Velez <jvelez20@yahoo.com>
2019-01-10 18:49:11 +0000

    Automatically backed up by Learn

--- test/index-test.js
+++ test/index-test.js
@@ -36,7 +36,7 @@ describe('logWhisper(string)', function() {
 })
 
 describe('sayHiToGrandma(string)', function() {
-  return('returns "I can\'t hear you!" if `string` is lowercase', function() {
+  console.log('returns "I can\'t hear you!" if `string` is lowercase', function() {
     expect(sayHiToGrandma('hello')).toEqual("I can't hear you!")
   })
 
